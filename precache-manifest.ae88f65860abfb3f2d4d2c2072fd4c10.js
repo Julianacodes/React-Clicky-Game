2a254645eaa44bf9a179efd68ba9cd9e57977fdc
Juliana Hernandez <juliana88h@gmail.com>
2020-08-27 00:49:20 -0700

    Updates

---
+++ precache-manifest.ae88f65860abfb3f2d4d2c2072fd4c10.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e16310b03129ec6ff3b0d1b5d3101a8",
+    "url": "/React-Clicky-Game/index.html"
+  },
+  {
+    "revision": "b99c04d16166df991707",
+    "url": "/React-Clicky-Game/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "c8fb274627be90dffe73",
+    "url": "/React-Clicky-Game/static/js/2.82e35678.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Clicky-Game/static/js/2.82e35678.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b99c04d16166df991707",
+    "url": "/React-Clicky-Game/static/js/main.f0a4341c.chunk.js"
+  },
+  {
+    "revision": "2f410e0f0e053906dab0",
+    "url": "/React-Clicky-Game/static/js/runtime-main.1d90443e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/React-Clicky-Game/static/media/logo.5d5d9eef.svg"
+  }
+]);
